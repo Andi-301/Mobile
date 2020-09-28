7f8b5b934e7995d3e49c63c9708060e65265a52a
Andika Hanavian Atmam <andikahanavian@gmail.com>
2020-09-28 13:03:16 +0700

    adding .gitignore as well as accomplishing lab 2

---
+++ HelloWorld/app/src/test/java/com/example/helloworld/MyUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.helloworld;
+
+import android.content.Context;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+
+public class MyUnitTest {
+    @Test
+    public void subtraction_isCorrect() {
+        assertEquals(1, 3 - 2);
+    }
+}
