62b4c31d5f9f4ff11caaefa8fefb2055aa36d345
Andika Hanavian Atmam <andikahanavian@gmail.com>
2020-10-04 13:34:10 +0700

    Adding Exit button and its function

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnexit = (Button) findViewById(R.id.exit);
+        btnexit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Exit Confirming");
+                builder.setMessage("Apa anda benar-benar ingin keluar?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i)
+                    {finish();}
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i)
+                    {dialogInterface.dismiss();}
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
     }
+
 }
