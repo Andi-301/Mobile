53a2439da35e9c01028a15573a6232b3c5b424bc
Andika Hanavian Atmam <andikahanavian@gmail.com>
2020-10-04 13:38:20 +0700

    Editing Exit button and its function

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnexit = (Button) findViewById(R.id.exit);
+        Button btnexit = (Button) findViewById(R.id.btn_exit);
         btnexit.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
