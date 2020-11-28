df496ec69207d05396df74c09ff72fe0f7bec7a6
Andika Hanavian Atmam <andikahanavian@gmail.com>
2020-11-28 21:06:26 +0700

    Importing utilities for wifi scanning

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,7 +1,21 @@
 package com.example.helloworld;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
