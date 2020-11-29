d1c848bf12797505e2a0fce849e2cd2396c8abb2
Andika Hanavian Atmam <andikahanavian@gmail.com>
2020-11-29 10:16:40 +0700

    adding wifi scanning function in MainActivity.java

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -20,9 +20,52 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WifiManager wifiManager;
+    private ListView listView;
+    private Button buttonScan;
+    private int size = 0;
+    private List<ScanResult> results;
+    private ArrayList<String> arrayList = new ArrayList<>();
+    private ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonScan = findViewById(R.id.scanButton);
+        buttonScan.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                scanTheWifi();
+            }
+        });
+        listView = findViewById(R.id.wifilist);
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        if(!wifiManager.isWifiEnabled()){
+            Toast.makeText(this, "Wifi is disabled... We need to enable it", Toast.LENGTH_LONG).show();
+            wifiManager.setWifiEnabled(true);
+        }
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(adapter);
+        scanTheWifi();
     }
+    private void scanTheWifi() {
+        arrayList.clear();
+        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        wifiManager.startScan();
+        Toast.makeText(this, "Scanning WiFi ...", Toast.LENGTH_SHORT).show();
+    }
+
+    BroadcastReceiver wifiReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            results = wifiManager.getScanResults();
+            unregisterReceiver(this);
+
+            for (ScanResult scanResult : results) {
+                arrayList.add(scanResult.SSID + " - " + scanResult.capabilities);
+                adapter.notifyDataSetChanged();
+            }
+        }
+    };
 }
