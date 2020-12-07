8ac309d041e625f3eb514a6c1a570ba4f232ebcc
Andika Hanavian Atmam <andikahanavian@gmail.com>
2020-12-07 13:35:31 +0700

    creating the native function for hello world (cont.)

--- HelloWorld/app/src/main/cpp/native-lib.cpp
+++ HelloWorld/app/src/main/cpp/native-lib.cpp
@@ -4,3 +4,9 @@
 extern "C" JNIEXPORT jstring
 
 JNICALL
+Java_com_example_helloworld_MainAcivity_stringfromJNI(
+        JNIEnv *env, jobject /* this */){
+    
+    std: jstring hello = (jstring) "Hello World from C++";
+    return env->NewStringUTF(hello->string());
+}
--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //useful for loading the 'native-lib' library on application startup
+    static {
+        System.loadlibrary("native-lib");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //How to calling the native method
+        Textview teks = (TextView) findViewById(R.id.sample_text);
+        teks.setText(stringFromJNI());
     }
+
+    public native String stringFromJNI();
 }
+
